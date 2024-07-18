298ed01b954254b0cb5cc7334927976e7321b084
Linkey550 <1539003346@QQ.COM>
2024-07-18 16:37:28 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,7 @@ uint16_t serverPort = 12222;         //服务器端口号
 
 
 WiFiServer server; //声明一个客户端对象，用于与服务器进行连接
-WiFiClient serverClients[MAX_SRV_CLIENTS];
 
- WebServer  esp32_server(80);  //声明一个 WebServer 的对象，对象的名称为 esp32_server
-                              //设置网络服务器响应HTTP请求的端口号为 80
 
 // put function declarations here:
 //void Setup_OTA(void);
@@ -31,10 +28,6 @@ WiFiClient serverClients[MAX_SRV_CLIENTS];
 void Task_OTA(void *pvParameters);
 void Task_Receive(void *pvParameters);
 
-//Web_task list:
-void handleRoot();
-void handleFound();
-void handleOpen();
 
 void setup() {
   // put your setup code here, to run once:
@@ -82,21 +75,13 @@ void setup() {
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());//输出本地ip
-  // Setup_OTA();
+
   
 
   server.begin(serverPort); //服务器启动监听端口号12222
   server.setNoDelay(true);
 
 
-  esp32_server.begin();  //启动网络服务器
-  esp32_server.on("/HolleWorld",HTTP_GET,handleRoot);  //函数处理当有HTTP请求 "/HolleWorld" 时执行函数 handleRoot 
-  esp32_server.on("/open",HTTP_GET,handleOpen);  //函数处理当有HTTP请求 "/HolleWorld" 时执行函数 handleRoot 
-  esp32_server.onNotFound(handleFound);  //当请求的网络资源不在服务器的时候，执行函数 handleFound 
-  
-
-
-  //Serial.printf("\nOTA_E\n");
   xTaskCreatePinnedToCore(Task_OTA, "Task_OTA", 10000, NULL, 1, NULL,  0);
   xTaskCreatePinnedToCore(Task_Receive, "Task_Receive", 10000, NULL, 1, NULL,  1);
 }
@@ -104,53 +89,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
  // Serial.printf("hello world");
- esp32_server.handleClient();
+ //esp32_server.handleClient();
 
 }
 
-// put function definitions here:
-
-// void Setup_OTA(void)
-// {
-//   Serial.println("Booting");
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin(ssid, password);
-//   while (WiFi.waitForConnectResult() != WL_CONNECTED)
-//   {
-//     Serial.println("Connection Failed! Rebooting...");
-//     delay(5000);
-//     ESP.restart();
-//   }
-//    ArduinoOTA
-//       .onStart([]()
-//                {
-//       String type;
-//       if (ArduinoOTA.getCommand() == U_FLASH)
-//         type = "sketch";
-//       else // U_SPIFFS
-//         type = "filesystem";
-
-//       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-//       Serial.println("Start updating " + type); })
-//       .onEnd([]()
-//              { Serial.println("\nEnd"); })
-//       .onProgress([](unsigned int progress, unsigned int total)
-//                   { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-//       .onError([](ota_error_t error)
-//                {
-//       Serial.printf("Error[%u]: ", error);
-//       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-//       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-//       else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-//       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-//       else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
-
-//   ArduinoOTA.begin();
-
-//   Serial.println("Ready");
-//   Serial.print("IP address: ");
-//   Serial.println(WiFi.localIP());
-// }
 
 void Task_OTA(void *pvParameters) {
   
@@ -196,7 +138,7 @@ void Task_Receive(void *pvParameters)
               {
                 //从Telnet客户端获取数据，并推送到URAT端口
                 while(serverClients[i].available()) {
-                  Serial.write(serverClients[i].read());
+                  Serial.println(serverClients[i].read());
                   vTaskDelay(1);
                 }
                 //vTaskDelay(1);
@@ -209,19 +151,3 @@ void Task_Receive(void *pvParameters)
 }
 
 
-void handleRoot()  
-{
-  Serial.print("客户端访问！");
-  esp32_server.send(200,"text/plain","Holle World!");  //用函数 send 向浏览器发送信息，200表示正常状态码，text/plain表示发送的内容为纯文本类型 text/html为HTML的网页信息，"Holle World!"为发送的内容
-}
-
-void handleFound()
-{
-  esp32_server.send(404,"text/plain","404:Not Found!");  //用函数 send 向浏览器发送信息，404表示服务器上找不到请求的资源，text/plain表示发送的内容为纯文本类型，"404：Not Found!"为发送的内容
-}
-
-void handleOpen()  
-{
-  Serial.print("客户端访问！");
-  esp32_server.send(200,"text/plain","open the door");  //用函数 send 向浏览器发送信息，200表示正常状态码，text/plain表示发送的内容为纯文本类型 text/html为HTML的网页信息，"Holle World!"为发送的内容
-}
